b410f27a6a82f5f03f79f056ea55365217a95de1
RuizeZ <imruize@126.com>
2024-07-31 21:57:05 +0800

    spring MVC Validation Integer Field Required

--- spring-boot-mvc/src/main/java/com/zeze/springboot/Customer.java
+++ spring-boot-mvc/src/main/java/com/zeze/springboot/Customer.java
@@ -1,7 +1,6 @@
 package com.zeze.springboot;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
@@ -12,4 +11,12 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min=1, message = "is required")
     private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value=0, message = "must be greater than or equal to zero")
+    @Max(value=10, message = "must be less than or equal to 10")
+    private Integer freePasses;
+
+    @Pattern(regexp="^[a-zA-Z0-9]{5}", message = "only 5 digits are allowed")
+    private String postalCode;
 }
--- spring-boot-mvc/src/main/java/com/zeze/springboot/CustomerController.java
+++ spring-boot-mvc/src/main/java/com/zeze/springboot/CustomerController.java
@@ -1,14 +1,21 @@
 package com.zeze.springboot;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/mvc")
 public class CustomerController {
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
     @GetMapping("/")
     public String showForm(Model theModel){
         theModel.addAttribute("customer", new Customer());
@@ -17,6 +24,7 @@ public class CustomerController {
 
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult){
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }else{
